931e223cff6ce366f66cd1581fa38c589cd104de
Marius Erdei <67762314+mariusdev91@users.noreply.github.com>
2021-02-21 11:27:13 +0200

    commit

--- script/main.js
+++ script/main.js
@@ -1,51 +1,50 @@
-function turnWordIntoArray() {
+function wordToArray() {
     const wordToGuess = document.querySelector('input').value;
     let letterArray = wordToGuess.split('');
-    const para = document.getElementById('wordArea');
-    for (let i = 0; i < letterArray.length; ++i) {
-        if (i !== letterArray.length - 1) {
-            para.innerText += '_' + ' ';
+    const wordArea = document.getElementById('wordArea');
+    letterArray.forEach(function (index) {
+        if (index !== letterArray.length - 1) {
+            wordArea.innerText += '_' + ' ';
         } else {
-            para.innerText += '_';
+            wordArea.innerText += '_';
         }
-    }
+    });
+}
+
+function winMessage() {
+    document.getElementById('score').innerText = `You have won! Congratulations! You had ${triesLeft} tries left. Want to try another game?`;
+    const newGame = document.createElement('button');
+    newGame.innerText = 'New Game';
+    document.getElementById('score').appendChild(newGame);
+    newGame.addEventListener('click', function () {
+        location.reload();
+    });
 }
 
 let triesLeft = 8;
-function checkForLetterOrWordMatch() {
+function comparingValues() {
     let letterOrWordwordToGuess = document.getElementById('guessArea').querySelector('input').value.toUpperCase();
     const wordToGuess = document.querySelector('input').value.toUpperCase();
     let modifiedWord = document.getElementById('wordArea').innerText.toUpperCase();
-    let para = '';
+    let wordArea = '';
     if (wordToGuess.indexOf(letterOrWordwordToGuess) !== -1 && modifiedWord.indexOf(letterOrWordwordToGuess) === -1 && letterOrWordwordToGuess.length < wordToGuess.length) {
         for (let j = 0; j < wordToGuess.length; ++j) {
             if (wordToGuess[j] === letterOrWordwordToGuess) {
-                para += letterOrWordwordToGuess;
+                wordArea += letterOrWordwordToGuess;
             } else {
-                para += modifiedWord[j];
+                wordArea += modifiedWord[j];
             }
         }
         document.getElementById('wordArea').innerText = '';
-        para = para.toUpperCase();
-        document.getElementById('wordArea').innerText = para;
-        alert(document.getElementById('wordArea').innerText + " " + wordToGuess);
+        wordArea = wordArea.toUpperCase();
+        document.getElementById('wordArea').innerText = wordArea;
         if (document.getElementById('wordArea').innerText.localeCompare(wordToGuess) === 0) {
-            document.getElementById('score').innerText = `You have won! Congratulations! You had ${triesLeft} tries left. Want to try another game?`;
-            const newGame = document.createElement('button');
-            newGame.innerText = 'New Game';
-            document.getElementById('score').appendChild(newGame);
-            newGame.addEventListener('click', function () {
-                location.reload();
-            });
+            winMessage();
         }
     } else if (wordToGuess.localeCompare(letterOrWordwordToGuess) === 0) {
         document.getElementById('wordArea').innerText = '';
         document.getElementById('wordArea').innerText = wordToGuess.toUpperCase();
-        document.getElementById('score').innerText = `You have won! Congratulations! You had ${triesLeft} tries left. Want to try another game?`;
-        const newGame = document.createElement('button');
-        newGame.innerText = 'New Game';
-        document.getElementById('score').appendChild(newGame);
-        newGame.addEventListener('click', function () { location.reload();});
+        winMessage();
     } else if (triesLeft > 0 && wordToGuess.indexOf(letterOrWordwordToGuess) === -1 || modifiedWord.indexOf(letterOrWordwordToGuess) !== -1) {
         document.getElementById('score').innerText = `You have ${triesLeft} tries left! Be careful!`;
         --triesLeft;
@@ -54,13 +53,14 @@ function checkForLetterOrWordMatch() {
     }
 }
 
-function init() {
+function startGame() {
     alert('Welcome to hangman! Good luck!')
     document.getElementById('myText').focus();
     const addWordButton = document.getElementById('addWord');
     const addLetterButton = document.getElementById('checkForLetter');
-    addWordButton.addEventListener('click', turnWordIntoArray);
-    addLetterButton.addEventListener('click', checkForLetterOrWordMatch);
+    addWordButton.addEventListener('click', wordToArray);
+    addLetterButton.addEventListener('click', comparingValues);
+    ++start;
 }
 
-init();
+startGame();
